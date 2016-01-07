c3df7f0b8706966183c6e7791f359b79b4a9cd75
Jess Portnoy <jess.portnoy@kaltura.com>
2016-01-07 20:12:00 +0000

    commit onebox plugin.

---
+++ _plugins/onebox.rb
@@ -0,0 +1,25 @@
+require "onebox"
+
+Onebox::Engine::WhitelistedGenericOnebox.whitelist << "www.kaltura.com"
+
+module Jekyll
+  class OneboxP < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+       super
+       @text = text
+    end
+    
+    def render(context)
+      # pipe param through liquid to make additional replacements possible
+      url = Liquid::Template.parse(@text).render context
+	#print url
+	#url ="http://videos.kaltura.com/media/1_is3qd1az"
+      # oembed look up
+      preview = Onebox.preview(url) 
+      "<div>#{preview}</div>"
+
+    end
+  end
+end
+
+Liquid::Template.register_tag('onebox', Jekyll::OneboxP)
