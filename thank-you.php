e3b3cd51520c6440dfa50b09e278479516fdc235
Zohar Babin <z.babin@gmail.com>
2016-05-14 13:59:10 -0500

    Update thank-you.php

--- thank-you.php
+++ thank-you.php
@@ -4,7 +4,7 @@ layout: default
 <script>
 $( document ).ready(function() {
     $('.thank-you-heading').fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).delay(6000).fadeOut(1000);
-};
+});
 </script>
 <div class="w-section thank-you-section">
     <div class="w-container">
